16de8438723d6d675bd4a819362cb24fcc4157a4
Dalemar Glenn <dale_197@outlook.com>
2023-12-21 15:56:18 -0500

    Dales Quick Fix

--- account/register.js
+++ account/register.js
@@ -1,6 +1,39 @@
-let username;
-let password;
-let fullName;
+"use strict";
+
+const emailInput = document.querySelector("#emailInput");
+const usernameInput = document.querySelector("#usernameInput");
+const passwordInput = document.querySelector("#passwordInput");
+const confirmPassword = document.querySelector("#confirmPassword");
+
+const submitButton = document.querySelector("#submitButton")
+
+submitButton.addEventListener("click", registerUser)
+
+
+function registerUser() {
+   if (passwordInput.value != confirmPassword.value) {
+      console.log("passwords didnt match");
+   }
+
+    if (
+        emailInput.value.trim() === "" ||
+      usernameInput.value.trim() === "" ||
+      passwordInput.value.trim() === "" ||
+      confirmPassword.value.trim() === ""
+   ) {
+      console.log("submit all fields before submitting");
+  
+   }
+
+   registerNewUser(emailInput, usernameInput, passwordInput);
+}
+
+
+
+function registerNewUser(emailInput, usernameInput, passwordInput) {
+   alert("Registration successful! You can now log in.");
+}
+
 
 async function createAccount() {
    let response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users", {
