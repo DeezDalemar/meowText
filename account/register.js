b2e1f79a09fc694d356af16f974bb49724658d8c
Dalemar Glenn <dale_197@outlook.com>
2023-12-21 13:54:33 -0500

    function

--- account/register.js
+++ account/register.js
@@ -1,3 +1,23 @@
+let username;
+let password;
+let fullName;
+
 async function createAccount() {
-    let response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users")
+   let response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users", {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+      },
+      body: JSON.stringify(getSignInData()),
+   });
+}
+
+function getSignInData() {
+   let userInfo = {
+      username: username.value,
+      fullName: fullName.value,
+      password: password.value,
+    }
+    
+    return userInfo
 }
